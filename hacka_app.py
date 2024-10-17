6322c640c0687e79a6800dd4bc9d7ca4c10407a9
rafael <121638250+RafeuLopo@users.noreply.github.com>
2024-10-17 15:00:44 -0300

    Update hacka_app.py

--- hacka_app.py
+++ hacka_app.py
@@ -280,14 +280,8 @@ def chat():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-def create_app():
+if __name__ == '__main__':
     app = Flask(__name__)
     CORS(app)
     app.register_blueprint(api, url_prefix="/api")
-
-    return app
-
-if __name__ == '__main__':
-
-    app = create_app()
     app.run(debug=True)
